035081f0d8abb4202bf2dc3d190b17c60405fafa
mfrancia <m.francia@unibo.it>
2026-08-26 11:45:15 +0200

    fix: add slides ci

--- slides/publish.sh
+++ slides/publish.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -exo
-quarto preview ageml.qmd --to revealjs --port 6778 --host 0.0.0.0
+# quarto preview ageml.qmd --to revealjs --port 6778 --host 0.0.0.0
+quarto render --to revealjs
+quarto render --to pptx
+quarto render --to pdf
